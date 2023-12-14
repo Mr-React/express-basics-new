8dfc961101913104d86438540bfe689b5d4b8c31
Mr-React <ambureomkar@gmail.com>
2023-12-14 15:39:30 +0530

    middleware with next param

--- README.md
+++ README.md
@@ -20,6 +20,10 @@
   ```
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+      console.log(`${req.method}:${req.url}`);
+      next();
+    });
   ```
 
 - example
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 // midllerwares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method}:${req.url}`);
+  next();
+});
 
 // local store
 const groceryList = [
